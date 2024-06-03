fcfd53fa89647f96e527769834214e9e8deb3af9
Abhipatil11 <abhishekwarude15174@gmail.com>
2024-06-03 15:56:48 +0530

    commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,19 @@
 import { Link } from "react-router-dom";
 import BgShape from "../images/hero/hero-bg.png";
 import HeroCar from "../images/hero/main-car.png";
+import Logo from "../images/phone.png"; // Assuming you want to use phone.png as logo
 import { useEffect, useState } from "react";
+import '../components/Hero.css'; // Custom CSS for z-index and other styles
 
 function Hero() {
   const [goUp, setGoUp] = useState(false);
 
   const scrollToTop = () => {
-    window.scrollTo({ top: (0, 0), behavior: "smooth" });
+    window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
   const bookBtn = () => {
-    document
-      .querySelector("#booking-section")
-      .scrollIntoView({ behavior: "smooth" });
+    document.querySelector("#booking-section").scrollIntoView({ behavior: "smooth" });
   };
 
   useEffect(() => {
@@ -30,53 +30,57 @@ function Hero() {
       window.removeEventListener("scroll", onPageScroll);
     };
   }, []);
+
   return (
-    <>
-      <section id="home" className="hero-section">
-        <div className="container">
-          <img className="bg-shape" src={BgShape} alt="bg-shape" />
-          <div className="hero-content">
-            <div className="hero-content__text">
-              <h1>JAY BHOLE</h1>
-              <h1>
-                TOURS <span>AND</span> TRAVELS our car rental
-              </h1>
-              <p>
-                Rent the car of your dreams. Unbeatable prices, unlimited miles,
-                flexible pick-up options and much more.
-              </p>
-              <div className="hero-content__text__btns">
-                <Link
-                  onClick={bookBtn}
-                  className="hero-content__text__btns__book-ride"
-                  to="/"
-                >
-                  Book Ride &nbsp; <i className="fa-solid fa-circle-check"></i>
-                </Link>
-                <Link className="hero-content__text__btns__learn-more" to="/">
-                  Learn More &nbsp; <i className="fa-solid fa-angle-right"></i>
-                </Link>
-              </div>
-            </div>
-
-            {/* img */}
-            <img
-              src={HeroCar}
-              alt="car-img"
-              className="hero-content__car-img"
-            />
+    <section id="home" className="hero-section">
+      <div className="container position-relative">
+        <img className="bg-shape position-absolute" src={BgShape} alt="bg-shape" />
+        <div className="hero-content text-center position-relative">
+          <div className="hero-content__header d-flex justify-content-between align-items-center mb-4 fixed-phone">
+            <a href="tel:8275829980">
+              <img src={Logo} alt="logo" className="hero-content__logo" />
+            </a>
+            <a href="tel:8275829980" className="hero-content__phone">Call Us: 8275829980</a>
           </div>
+          <div className="hero-content__text">
+            <h1>JAY BHOLE</h1>
+            <h1>
+              TOURS <span>AND</span> TRAVELS our car rental
+            </h1>
+            <p>
+              Rent the car of your dreams. Unbeatable prices, unlimited miles,
+              flexible pick-up options and much more.
+            </p>
+            <div className="hero-content__text__btns d-flex justify-content-center">
+              <Link
+                onClick={bookBtn}
+                className="btn btn-primary mr-3"
+                to="/"
+              >
+                Book Ride &nbsp; <i className="fa-solid fa-circle-check"></i>
+              </Link>
+              <Link className="btn btn-secondary" to="/">
+                Learn More &nbsp; <i className="fa-solid fa-angle-right"></i>
+              </Link>
+              <a className="btn btn-secondary" href="tel:8275829980">
+                Call Us &nbsp; <i className="fa-solid fa-phone"></i>
+              </a>
+              <a className="btn btn-secondary" href="tel:1234567890">
+                Call Us &nbsp; <i className="fa-solid fa-phone"></i>
+              </a>
+            </div>
+          </div>
+          <img src={HeroCar} alt="car-img" className="hero-content__car-img img-fluid mt-4" />
         </div>
-
-        {/* page up */}
-        <div
-          onClick={scrollToTop}
-          className={`scroll-up ${goUp ? "show-scroll" : ""}`}
-        >
-          <i className="fa-solid fa-angle-up"></i>
-        </div>
-      </section>
-    </>
+      </div>
+      <div
+        onClick={scrollToTop}
+        className={`scroll-up ${goUp ? "show-scroll" : ""} position-fixed`}
+        style={{ zIndex: 9999 }}
+      >
+        <i className="fa-solid fa-angle-up"></i>
+      </div>
+    </section>
   );
 }
 
